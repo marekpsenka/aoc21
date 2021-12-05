08e3f7bb32dd657e3eaf89e2b087c612913f0f67
Marek Pšenka <psenka@edhouse.cz>
2021-12-05 10:36:12 +0100

    Day 05

---
+++ day05a.py
@@ -0,0 +1,33 @@
+import fileinput
+
+input = map(str.rstrip, fileinput.input())
+
+m = dict()
+
+for line in input:
+    [bs, es] = line.split(" -> ")
+    (x1, y1) = tuple(map(int, bs.split(",")))
+    (x2, y2) = tuple(map(int, es.split(",")))
+    if x1 == x2:
+        for i in range(min(y1, y2), max(y1, y2) + 1):
+            if (not (x1, i) in m):
+                m[(x1, i)] = 1
+            else:
+                m[(x1, i)] = m[(x1, i)] + 1
+    elif y1 == y2:
+        for i in range(min(x1, x2), max(x1, x2) + 1):
+            if (not (i, y1) in m):
+                m[(i, y1)] = 1
+            else:
+                m[(i, y1)] = m[(i, y1)] + 1
+
+count = 0
+
+for v in m.values():
+    if v > 1:
+        count += 1
+
+print(count)
+
+
+
